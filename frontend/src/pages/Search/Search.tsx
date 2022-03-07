662b8e2c36a600107f3e67d8f1aaf96a83503e64
MT-0xf <matsumoto0xf@gmail.com>
2022-03-08 00:01:17 +0900

    ページネーション 導入

--- frontend/src/pages/Search/Search.tsx
+++ frontend/src/pages/Search/Search.tsx
@@ -4,13 +4,24 @@ import './Search.css';
 import axios from "axios";
 import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from "react";
+import MuiPagination from '@material-ui/lab/Pagination';
+import { withStyles } from '@material-ui/core/styles';
 
 function Search() {
   const search = useLocation().search;
   const query = new URLSearchParams(search);
   const keyword = query.get('keyword');
   const [data, setData] = useState([]);
+  let allList;
   let list;
+  let max: number = 15;
+
+  const [page, setPage] = useState(1)
+  const Pagination = withStyles({
+    root: {
+      display: 'inline-block',  //中央寄せのためインラインブロックに変更
+    },
+  })(MuiPagination);  
 
   useEffect(() => {
     axios.get('http://localhost:3000/books/search?keyword=' + keyword).then(
@@ -20,7 +31,7 @@ function Search() {
     );
   }, []);
 
-  list = data.map((value: { id: number; title: string; author_name: string; page_number: string; file: string;}, key) => {
+  allList = data.map((value: { id: number; title: string; author_name: string; page_number: string; file: string;}, key) => {
     return <BookCard key={key} 
                     id={value.id} 
                     title={value.title}
@@ -29,6 +40,9 @@ function Search() {
                     file={value.file} />
   })
 
+  let totalPage: number = Math.ceil(allList.length / max);
+  list = allList.slice((page - 1) * max, page * max);
+  
   return (
     <div>
       <Header />
@@ -36,6 +50,16 @@ function Search() {
         <div className="head">「{keyword}」の検索結果</div>
         <div className="book-list">
           {list}
+        </div>
+        <div style={{textAlign: "center"}}>
+          <Pagination 
+            count={totalPage}
+            color="primary"
+            onChange={(e, page) =>{
+              setPage(page);
+            }}
+            page={page}
+          />
         </div>
       </div>
     </div>
